64c63777c4ef61115c44a5995b7c6b014f62cfd6
hbayrakt <hbayrakt@mail.ccsf.edu>
2021-10-21 22:32:58 -0400

    Add db connection to __init__: AK

--- MovieRecommender/user/__init__.py
+++ MovieRecommender/user/__init__.py
@@ -2,3 +2,13 @@ from flask import Flask
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+
+try:
+    mongo = pymongo.MongoClient(
+        host="localhost", 
+        port=27017, 
+        serverSelectionTimeoutMS=3000)
+    db = mongo.movie
+    mongo.server_info() # if cannot connect to db, will get an exception
+except:
+    print("ERROR: Cannot connect to database")
