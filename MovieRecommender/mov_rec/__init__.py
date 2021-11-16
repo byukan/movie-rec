6dfd11438ef75ec50000d9630f17442bd7921496
Annat Koren <annat.koren@gmail.com>
2021-11-16 01:34:11 -0800

    Update mongo endpoint

--- MovieRecommender/mov_rec/__init__.py
+++ MovieRecommender/mov_rec/__init__.py
@@ -8,10 +8,13 @@ app = Flask(__name__)
 bcrypt = Bcrypt(app)
 
 try:
-    mongodb_client = MongoClient(host='localhost', port=27017, connectTimeoutMS=5000)
-    db = mongodb_client.stub
-    mongodb_client.admin.command('ping') # if cannot connect to database, will get an exception
+    # mongodb_client = MongoClient(host='localhost', port=27017, connectTimeoutMS=5000)
+    mongodb_client = MongoClient("mongodb+srv://m001-student:3@r.vu0ut.mongodb.net/movie?retryWrites=true&w=majority",
+                                 connectTimeoutMS=5000)
+    db = mongodb_client.movie
+    mongodb_client.admin.command('ping')  # if cannot connect to database, will get an exception
 except ConnectionFailure:
     print("ERROR: Server unavailable")
+
 
 from mov_rec import database, models, routes, search, view_movie
