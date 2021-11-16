8cebf0537eb9fbe309168bb97edeecda12b46ce0
Annat Koren <annat.koren@gmail.com>
2021-11-16 01:37:59 -0800

    Minor cleanup, routes.py

--- MovieRecommender/mov_rec/routes.py
+++ MovieRecommender/mov_rec/routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request ##, session
+from flask import Flask, render_template, request  # , session
 from mov_rec import app, database
 from mov_rec.models import User
 
@@ -13,21 +13,21 @@ def home():
     return render_template ('home.html')
 
 
-@app.route("/login/") # not yet implemented
+@app.route("/login/")  # not yet implemented
 def login():
-    return render_template ('login.html')
+    return render_template('login.html')
 
 
-@app.route("/signup/", methods=['GET', 'POST']) # POST not completely implemented
+@app.route("/signup/", methods=['GET', 'POST'])  # POST not completely implemented
 def signup():
     if request.method == 'POST':
         return User().signup()
-    return render_template ('signup.html') # method is 'GET'
+    return render_template('signup.html')  # method is 'GET'
 
 
 @app.route("/usersfeed/")
 def usersfeed():
-    return render_template ('usersfeed.html')
+    return render_template('usersfeed.html')
 
 
 def _print_debug_statement(description, element, module):
