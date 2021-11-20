1acd8158d7e4ed7398319497925d526a2479a8aa
Annat Koren <annat.koren@gmail.com>
2021-11-20 11:45:52 -0800

    Add hard-coded similar movies to movie retrieval

--- MovieRecommender/mov_rec/database.py
+++ MovieRecommender/mov_rec/database.py
@@ -31,15 +31,30 @@ def movie_info(movie_id):
     try:
         # movie_count = db.movies.count_documents({})
         # print("movie count:", movie_count)
-
         # print("searching for movie with id:", movie_id)
 
         # results = list(db.movies.find({'_id': movie_id}))
         result = db.movies.find_one({'_id': movie_id})
 
-        # if result:  # for result in results:
-        #     for item in result:
-        #         print(item + ":", result[item])
+        # hardcoded similar movies
+        if result:
+            result["similar_movies"] = [
+                {
+                    '_id': 502,
+                    'Series_Title': "Iron Man",
+                    'Poster_Link': 'https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@.jpg'
+                },
+                {
+                    '_id': 103,
+                    'Series_Title': "Reservoir Dogs",
+                    'Poster_Link': 'https://m.media-amazon.com/images/M/MV5BZmExNmEwYWItYmQzOS00YjA5LTk2MjktZjEyZDE1Y2QxNjA1XkEyXkFqcGdeQXVyMTQxNzMzNDI@.jpg'
+                },
+                {
+                    '_id': 900,
+                    'Series_Title': "The Raid",
+                    'Poster_Link': 'https://m.media-amazon.com/images/M/MV5BZGIxODNjM2YtZjA5Mi00MjA5LTk2YjItODE0OWI5NThjNTBmXkEyXkFqcGdeQXVyNzQ1ODk3MTQ@.jpg'
+                }
+            ]
 
     except Exception as ex:
         print(ex)
