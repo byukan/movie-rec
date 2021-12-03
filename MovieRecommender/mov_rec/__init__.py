9799d4064995f816f9bfca06243330cbd15dbc05
Annat Koren <annat.koren@gmail.com>
2021-12-02 19:47:10 -0800

    Fix pymongo handshake

--- MovieRecommender/mov_rec/__init__.py
+++ MovieRecommender/mov_rec/__init__.py
@@ -2,15 +2,16 @@ from flask import Flask
 from pymongo import MongoClient
 from pymongo.errors import ConnectionFailure
 from flask_bcrypt import Bcrypt
+import certifi
 
 
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
 
 try:
-    # mongodb_client = MongoClient(host='localhost', port=27017, connectTimeoutMS=5000)
+    ca = certifi.where()
     mongodb_client = MongoClient("mongodb+srv://m001-student:3@r.vu0ut.mongodb.net/movie?retryWrites=true&w=majority",
-                                 connectTimeoutMS=5000)
+                                 tlsCAFile=ca, connectTimeoutMS=5000)
     db = mongodb_client.movie
     mongodb_client.admin.command('ping')  # if cannot connect to database, will get an exception
 except ConnectionFailure:
