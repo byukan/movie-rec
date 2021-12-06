561b7188a87569d7362945f57f930c55d53969f6
Annat Koren <annat.koren@gmail.com>
2021-12-05 23:37:01 -0800

    Revise route, remove print statements

--- MovieRecommender/mov_rec/home.py
+++ MovieRecommender/mov_rec/home.py
@@ -4,7 +4,6 @@ from mov_rec import app, database
 @app.route("/", methods=['GET', 'POST'])
 @app.route("/home/", methods=['GET', 'POST'])
 def search():
-    print("Request method:", request.method)
 
     if request.method == 'POST':
         search_term = request.form["search_term"]
--- MovieRecommender/mov_rec/view_movie.py
+++ MovieRecommender/mov_rec/view_movie.py
@@ -2,13 +2,12 @@ from flask import Flask, render_template, request
 from mov_rec import app, database
 
 
-@app.route("/view_movie", methods=['GET', 'POST'])  # POST is for liking or favoriting a movie; not implemented yet
+@app.route("/view_movie", methods=['GET'])
 def view_movie():
     movie_id = -1
     movie_id_string = request.args.get('id')  # if no ID parameter, movie_id_string will be None
     if movie_id_string:
         movie_id = int(movie_id_string)
-    print('url param & id:', movie_id_string, movie_id)
 
     movie, next_id = database.movie_info(movie_id, True)
     return render_template('view_movie.html', movie=movie, next_id=next_id)
