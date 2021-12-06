18dbb5742a50c7f49f725d8bfe55624716d8775f
Annat Koren <annat.koren@gmail.com>
2021-12-05 23:27:31 -0800

    Require search term

--- MovieRecommender/mov_rec/home.py
+++ MovieRecommender/mov_rec/home.py
@@ -6,12 +6,11 @@ from mov_rec import app, database
 def search():
     print("Request method:", request.method)
 
-    if request.method == 'GET' or not request.form["search_term"]:  # want to ignore ""
-        movies = database.highest_rated_movies()
-
-    else:  # method is POST with a search term
+    if request.method == 'POST':
         search_term = request.form["search_term"]
-        print("Search term:", search_term)
         movies = database.search_movies(search_term)
+
+    else:
+        movies = database.highest_rated_movies()
 
     return render_template('home.html', movies=movies)
