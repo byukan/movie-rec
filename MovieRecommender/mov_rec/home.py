acd451b98cc05b37b49615a2567e9223e1084f12
Annat Koren <annat.koren@gmail.com>
2021-12-06 16:27:48 -0800

    Reformat home.py

--- MovieRecommender/mov_rec/home.py
+++ MovieRecommender/mov_rec/home.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from mov_rec import app, database
 
+
 @app.route("/", methods=['GET', 'POST'])
 @app.route("/home/", methods=['GET', 'POST'])
 def search():
