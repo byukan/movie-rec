f75a1697490177a1aacfd6142fe98031782354a5
Annat Koren <annat.koren@gmail.com>
2021-12-14 23:20:32 -0800

    Add movie name integration test, allow all tests: AB

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -17,52 +17,49 @@ it ("search garbage", function (){
     
 })
 
-it.only ("movie buttons test", function (){
+
+it ("movie buttons test", function (){
 
     cy.visit('http://localhost:5000/view_movie')
     cy.contains('Show me a different movie').click()
-    
-    
 
 })
 
-it.only ("movie buttons test", function (){
+it ("movie buttons test", function (){
 
     cy.visit('http://localhost:5000/view_movie')
     cy.contains('Go Back to Search Page').click()
-    
-    
 
 })
 
-
-it.only ("movie buttons test", function (){
+it ("movie buttons test", function (){
 
     cy.visit('http://localhost:5000/view_movie?id=345')
     cy.contains('Show Me a Different Movie').click()
-    
-    
 
 })
 
-it.only ("movie buttons test", function (){
+it ("movie buttons test", function (){
 
     cy.visit('http://localhost:5000/view_movie?id=345')
     cy.contains('Go Back to Search Page').click()
-    
-    
 
 })
 
 
-
-it("movie name test", function (){
+it("movie info test", function (){
 
     cy.visit('http://localhost:5000/view_movie?id=496')
     cy.get('.movie_name').should('be.visible')
 
 })
 
+it.only("movie name test", function (){
+
+    cy.visit('http://localhost:5000/view_movie?id=496')
+    cy.get('.movie_name').should('be.visible').contains('Letters from Iwo Jima')
+
+})
 
 it ("movie poster test", function (){
 
@@ -70,4 +67,3 @@ it ("movie poster test", function (){
     cy.get('.imgBx > img').should('have.css', 'width', '300px' )
 
 })
-
